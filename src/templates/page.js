d6dc0b3b7a0fe0f15aa425c778affca18926945d
elisejakob <hei@elisejakob.no>
2019-01-30 14:07:51 +0100

    simplify markup

--- src/templates/page.js
+++ src/templates/page.js
@@ -5,22 +5,12 @@ import Layout from '../components/Layout'
 
 export const PageTemplate = ({ title, content }) => {
   return (
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                {title}
-              </h2>
-              <div
-                className="content"
-                dangerouslySetInnerHTML={{ __html: content }}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
+    <section className="page">
+      <h1 className="section-title">{title}</h1>
+      <div
+        className="content"
+        dangerouslySetInnerHTML={{ __html: content }}
+      />
     </section>
   )
 }

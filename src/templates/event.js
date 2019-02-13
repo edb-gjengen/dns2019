221b8b4eb09a421ecc5f35d3a8885bb516e773bc
Jonas Braathen <jonas@ponas.no>
2019-02-13 01:59:01 +0100

    Fetch more fields for events. Show the featured image

--- src/templates/event.js
+++ src/templates/event.js
@@ -3,16 +3,47 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
+import Img from 'gatsby-image'
 
 export const EventTemplate = ({
   content,
   title,
+  featuredMedia,
+  startTime,
+  endTime,
+  venue,
+  facebookUrl,
+  ticketUrl,
+  priceStudent,
+  priceOrdinary
 }) => {
   return (
     <section className="event">
         <h1 className="title">
             {title}
         </h1>
+        <div className="featured-image">
+          <Img fluid={featuredMedia.localFile.childImageSharp.fluid} />
+        </div>
+        <div classname="start-time">
+            Start: {startTime}
+        </div>
+        <div className="end-time">
+            Slutt: {endTime}
+        </div>
+        <div className="venue">
+            Lokale: {venue}
+        </div>
+        <div className="facebook-url">
+            Facebook: {facebookUrl}
+        </div>
+        <div className="price">
+            Student: {priceStudent}
+            Ordinær: {priceOrdinary}
+        </div>
+        <div className="tickets">
+            Kjøp billett: {ticketUrl}
+        </div>
         <div dangerouslySetInnerHTML={{ __html: content }} />
     </section>
   )
@@ -21,6 +52,7 @@ export const EventTemplate = ({
 EventTemplate.propTypes = {
   content: PropTypes.node.isRequired,
   title: PropTypes.string,
+  featuredImage: PropTypes.object,
 }
 
 const Event = ({ data }) => {
@@ -30,12 +62,20 @@ const Event = ({ data }) => {
     <Layout>
       <Helmet title={`${event.title}`} />
       <EventTemplate
+        title={event.title}
         content={event.content}
+        featuredMedia={event.featured_media}
+        startTime={event.start_time}
+        endTime={event.start_time}
+        venue={event.venue}
+        facebookUrl={event.facebook_url}
+        ticketUrl={event.ticket_url}
+        priceStudent={event.price_member}
+        priceRegular={event.price_regular}
         // categories={post.categories}
         // tags={post.tags}
-        title={event.title}
         // date={post.date}
-        //author={post.author}
+        // author={post.author}
       />
     </Layout>
   )
@@ -62,8 +102,22 @@ export const eventQuery = graphql`
       title
       slug
       content
-      start_time(formatString: "MMMM DD, YYYY")
-      end_time(formatString: "MMMM DD, YYYY")
+      start_time
+      end_time
+      venue
+      facebook_url
+      ticket_url
+      price_member
+      price_regular
+      featured_media {
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 800) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
     }
   }
 `

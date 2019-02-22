eacec6e8243f44ed1d35e78f4929a24d356a2dac
Jonas Braathen <jonas@ponas.no>
2019-02-22 01:44:22 +0100

    Fetch event types, link them with event nodes

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
         includedRoutes: [
           "**/*/*/categories",
           "**/*/*/events",
+          "**/*/*/event_type",
           "**/*/*/posts",
           "**/*/*/pages",
           "**/*/*/media",
--- src/wordpress.js
+++ src/wordpress.js
@@ -4,6 +4,7 @@ const htmlEntities = new Entities();
 
 const neufNormalizer = ({ entities }) => {
   entities = decodeTitles(entities)
+  entities = mapEventsToEventTypes(entities)
   return entities
 }
 
@@ -14,6 +15,28 @@ const decodeTitles = entities => {
     if (e.title) {
       e.title = htmlEntities.decode(e.title)
     }
+    return e
+  })
+}
+
+const mapEventsToEventTypes = entities => {
+  const eventTaxonomies = [`wordpress__wp_event_type`, ]
+  const eventTypes = entities.filter(e => eventTaxonomies.includes(e.__type))
+
+  return entities.map(e => {
+    // Replace event types with links to their nodes.
+    let eventHasType = e.event_type && Array.isArray(e.event_type) && e.event_type.length
+    if (eventTypes.length && eventHasType) {
+      e.event_type___NODE = e.event_type.map(
+        t =>
+          eventTypes.find(
+            tObj =>
+              (Number.isInteger(t) ? t : t.wordpress_id) === tObj.wordpress_id
+          ).id
+      )
+      delete e.event_type
+    }
+
     return e
   })
 }

847efe57a8a6aad11af553fc068d7f88a07a8d67
elisejakob <hei@elisejakob.no>
2019-08-06 00:43:27 +0200

    finishing touches before launch

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,30 +22,15 @@ export default class IndexPage extends React.Component {
         />
         <section className="front-page-info">
           <div className="visit-us">
-            <div className="inner">
-              <h2 className="section-title">Besøk oss</h2>
-              <div className="address">
-                <p>Vi holder til på Majorstua, i Slemdalsveien 15.</p>
-                <a href="https://www.google.no/maps/place/Slemdalsveien+15,+0369+Oslo/data=!4m2!3m1!1s0x46416dc4ab53aa7d:0x5629681f36c1c8af?sa=X&ved=2ahUKEwjR94re3unjAhUj4aYKHRLOAuwQ8gEwAHoECBQQAQ" target="_blank" className="button button-small">Se i Google Maps</a>
-              </div>
-              <div className="opening-hours">
-                <ul>
-                  <li>
-                    <span class="opening-hours_venue">Glassbaren</span>
-                    <ul>
-                      <li>Mandag–lørdag: 14:00–00:00</li>
-                    </ul>
-                  </li>
-                  <li>
-                  <span class="opening-hours_venue">Bokcaféen</span>
-                    <ul>
-                      <li>Mandag–torsdag: 19:00–00:00</li>
-                      <li>Fredag: 20:00–03:00</li>
-                    </ul>
-                  </li>
-                </ul>
-                <a href="#" className="button button-small">Se alle åpningstider</a>
-              </div>
+            <h2 className="section-title">Besøk oss</h2>
+            <p>Chateau Neuf ligger på Majorstua i Oslo, i <a href="https://www.google.no/maps/place/Slemdalsveien+15,+0369+Oslo/data=!4m2!3m1!1s0x46416dc4ab53aa7d:0x5629681f36c1c8af?sa=X&ved=2ahUKEwjR94re3unjAhUj4aYKHRLOAuwQ8gEwAHoECBQQAQ" target="_blank">Slemdalsveien 15</a>.</p>
+            <div className="visit-us_links">
+              <Link to="/bli-aktiv/" className="button button-small">
+                Praktisk informasjon
+              </Link><br />
+              <Link to="/bli-aktiv/" className="button button-small">
+                Åpningstider
+              </Link>
             </div>
           </div>
           <div className="volunteer">
